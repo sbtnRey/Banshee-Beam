1e933e16ec0d44d8eb4b6688d5ea116625d5e51e
Sebastian Reynolds <sxr@pdx.edu>
2019-06-05 14:25:48 -0700

    Initail code
    Demo scene + main base for the banshee component. Demo scene utilizes gradient projectile

---
+++ Plugins/PitchDetector/Components/PitchDetector.cs
@@ -0,0 +1,182 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace FinerGames.PitchDetector
+{
+    public class PitchDetector : MonoBehaviour
+    {
+        //public GameObject projectile;
+        public AudioSource Source;
+        public float Pitch;
+        public int MidiNote;
+        public int MidiCents;
+        
+        //public GameObject test;
+        //public GameObject grad_0;
+
+        public GameObject grad_1;
+        public GameObject grad_2;
+        public GameObject grad_3;
+        public GameObject grad_4;
+        public GameObject grad_5;
+        public GameObject grad_6;
+        public GameObject grad_7;
+        public GameObject grad_8;
+        public GameObject grad_9;
+        public GameObject grad_10;
+        public GameObject grad_11;
+        public GameObject grad_12;
+        public GameObject grad_13;
+        
+        //public GameObject projectile;
+
+        public float delay;
+        public float force;
+
+        public Transform spawn;
+        public float fireRate = 0.5F;
+        private float nextFire = 0.0F;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+
+            Debug.Log(Pitch);
+            /*
+            if (Pitch > 20 && Time.time > nextFire)
+            {
+                nextFire = Time.time + fireRate;
+                GameObject bullet = Instantiate(grad_1, transform.position,  Quaternion.identity) as GameObject;
+                bullet.GetComponent<Rigidbody>().AddForce(transform.forward * force);
+                Destroy(bullet, delay);
+            }
+            */
+
+            
+            if ((Pitch > 20 && Pitch < 40) && Time.time > nextFire)
+            {
+                nextFire = Time.time + fireRate;
+                GameObject bullet = Instantiate(grad_1, transform.position,  Quaternion.identity) as GameObject;
+                bullet.GetComponent<Rigidbody>().AddForce(transform.forward * force);
+                Destroy(bullet, delay);
+            }
+
+            if ((Pitch > 40 && Pitch < 62) && Time.time > nextFire)
+            {
+                nextFire = Time.time + fireRate;
+                GameObject bullet = Instantiate(grad_1, transform.position,  Quaternion.identity) as GameObject;
+                bullet.GetComponent<Rigidbody>().AddForce(transform.forward * force);
+                Destroy(bullet, delay);
+            }
+
+
+            if ((Pitch > 62 && Pitch < 84) && Time.time > nextFire)
+            {
+                nextFire = Time.time + fireRate;
+                GameObject bullet = Instantiate(grad_2, transform.position,  Quaternion.identity) as GameObject;
+                bullet.GetComponent<Rigidbody>().AddForce(transform.forward * force);
+                Destroy(bullet, delay);
+            }
+
+
+            if ((Pitch > 84 && Pitch < 106) && Time.time > nextFire)
+            {
+                nextFire = Time.time + fireRate;
+                GameObject bullet = Instantiate(grad_3, transform.position,  Quaternion.identity) as GameObject;
+                bullet.GetComponent<Rigidbody>().AddForce(transform.forward * force);
+                Destroy(bullet, delay);
+            }
+
+            if ((Pitch > 106 && Pitch < 128) && Time.time > nextFire)
+            {
+                nextFire = Time.time + fireRate;
+                GameObject bullet = Instantiate(grad_4, transform.position,  Quaternion.identity) as GameObject;
+                bullet.GetComponent<Rigidbody>().AddForce(transform.forward * force);
+                Destroy(bullet, delay);
+            }
+
+
+            if ((Pitch > 128 && Pitch < 150) && Time.time > nextFire)
+            {
+                nextFire = Time.time + fireRate;
+                GameObject bullet = Instantiate(grad_5, spawn.position,  spawn.rotation) as GameObject;
+                bullet.GetComponent<Rigidbody>().AddForce(transform.forward * force);
+                Destroy(bullet, delay);
+            }
+
+            if ((Pitch > 150 && Pitch < 172) && Time.time > nextFire)
+            {
+                nextFire = Time.time + fireRate;
+                GameObject bullet = Instantiate(grad_6, transform.position,  Quaternion.identity) as GameObject;
+                bullet.GetComponent<Rigidbody>().AddForce(transform.forward * force);
+                Destroy(bullet, delay);
+            }
+
+            if ((Pitch > 172 && Pitch < 195) && Time.time > nextFire)
+            {
+                nextFire = Time.time + fireRate;
+                GameObject bullet = Instantiate(grad_7, transform.position,  Quaternion.identity) as GameObject;
+                bullet.GetComponent<Rigidbody>().AddForce(transform.forward * force);
+                Destroy(bullet, delay);
+            }
+
+            if ((Pitch > 195 && Pitch < 217) && Time.time > nextFire)
+            {
+                nextFire = Time.time + fireRate;
+                GameObject bullet = Instantiate(grad_8, transform.position,  Quaternion.identity) as GameObject;
+                bullet.GetComponent<Rigidbody>().AddForce(transform.forward * force);
+                Destroy(bullet, delay);
+            }
+
+            if ((Pitch > 239 && Pitch < 261) && Time.time > nextFire)
+            {
+                nextFire = Time.time + fireRate;
+                GameObject bullet = Instantiate(grad_9, transform.position,  Quaternion.identity) as GameObject;
+                bullet.GetComponent<Rigidbody>().AddForce(transform.forward * force);
+                Destroy(bullet, delay);
+            }
+
+            if ((Pitch > 261 && Pitch < 283) && Time.time > nextFire)
+            {
+                nextFire = Time.time + fireRate;
+                GameObject bullet = Instantiate(grad_10, transform.position,  Quaternion.identity) as GameObject;
+                bullet.GetComponent<Rigidbody>().AddForce(transform.forward * force);
+                Destroy(bullet, delay);
+            }
+
+            if ((Pitch > 283 && Pitch < 305) && Time.time > nextFire)
+            {
+                nextFire = Time.time + fireRate;
+                GameObject bullet = Instantiate(grad_11, transform.position,  Quaternion.identity) as GameObject;
+                bullet.GetComponent<Rigidbody>().AddForce(transform.forward * force);
+                Destroy(bullet, delay);
+            }
+
+            if ((Pitch > 305 && Pitch < 350) && Time.time > nextFire)
+            {
+                nextFire = Time.time + fireRate;
+                GameObject bullet = Instantiate(grad_12, transform.position,  Quaternion.identity) as GameObject;
+                bullet.GetComponent<Rigidbody>().AddForce(transform.forward * force);
+                Destroy(bullet, delay);
+            }
+
+            if ((Pitch > 350 && Pitch < 390) && Time.time > nextFire)
+            {
+                nextFire = Time.time + fireRate;
+                GameObject bullet = Instantiate(grad_13, transform.position,  Quaternion.identity) as GameObject;
+                bullet.GetComponent<Rigidbody>().AddForce(transform.forward * force);
+                Destroy(bullet, delay);
+            }
+            
+
+        }
+    }
+}
---
+++ Plugins/PitchDetector/Components/sineProjectile.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sineProjectile : MonoBehaviour
+{
+
+
+    public float MoveSpeed = 5.0f;
+
+    public float frequency = 10.0f;  // Speed of sine movement
+    public float magnitude = 0.5f;   // Size of sine movement
+    
+    private Vector3 axis;
+
+    private Vector3 pos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pos = transform.position;
+        Destroy(gameObject, 2.0f); // float is time till destroyed
+        axis = Vector3.right;  // May or may not be the axis you want
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+        pos += transform.up * Time.deltaTime * MoveSpeed;
+        transform.position = pos + axis * Mathf.Sin(Time.time * frequency) * magnitude;
+    }
+}
